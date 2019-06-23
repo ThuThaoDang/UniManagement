e1720f476c05313099c68b6d401ee7c81fb05624
nmtam <nmtam1004@gmail.com>
2019-06-24 04:42:26 +0700

    Create Models

--- app/Department.php
+++ app/Department.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+    public function courses()
+    {
+        return $this->hasMany('App\Course');
+    }
+
+    public function instructors()
+    {
+        return $this->hasMany('App\Instructor');
+    }
+
+    public function students()
+    {
+        return $this->hasMany('App\Student');
+    }
 }
--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    public function department()
+    {
+        return $this->belongsTo('App\Department');
+    }
+
+    public function instructor()
+    {
+        return $this->belongsTo('App\Instructor');
+    }
 }
